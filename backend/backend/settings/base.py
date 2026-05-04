9eaa7e5d18eae13af8334f0c7cf80ef0e576a14d
Aria Taylor <ari@aricodes.net>
2026-05-03 18:21:29 -0700

    Add drf-standardized-errors

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -86,4 +86,24 @@ class Base(Configuration):
     SPECTACULAR_SETTINGS = {
         "TITLE": "Ovum API",
         "VERSION": "0.1.0",
+        "POSTPROCESSING_HOOKS": [
+            "drf_standardized_errors.openapi_hooks.postprocess_schema_enums",
+            "drf_spectacular.contrib.djangorestframework_camel_case.camelize_serializer_fields",
+        ],
+        "SERVE_INCLUDE_SCHEMA": False,
+        "COMPONENT_SPLIT_REQUEST": True,
+        "CAMELIZE_NAMES": False,  # Perform this with POSTPROCESSING_HOOKS instead
+        "ENUM_NAME_OVERRIDES": {
+            "ValidationErrorEnum": "drf_standardized_errors.openapi_serializers.ValidationErrorEnum.choices",
+            "ClientErrorEnum": "drf_standardized_errors.openapi_serializers.ClientErrorEnum.choices",
+            "ServerErrorEnum": "drf_standardized_errors.openapi_serializers.ServerErrorEnum.choices",
+            "ErrorCode401Enum": "drf_standardized_errors.openapi_serializers.ErrorCode401Enum.choices",
+            "ErrorCode403Enum": "drf_standardized_errors.openapi_serializers.ErrorCode403Enum.choices",
+            "ErrorCode404Enum": "drf_standardized_errors.openapi_serializers.ErrorCode404Enum.choices",
+            "ErrorCode405Enum": "drf_standardized_errors.openapi_serializers.ErrorCode405Enum.choices",
+            "ErrorCode406Enum": "drf_standardized_errors.openapi_serializers.ErrorCode406Enum.choices",
+            "ErrorCode415Enum": "drf_standardized_errors.openapi_serializers.ErrorCode415Enum.choices",
+            "ErrorCode429Enum": "drf_standardized_errors.openapi_serializers.ErrorCode429Enum.choices",
+            "ErrorCode500Enum": "drf_standardized_errors.openapi_serializers.ErrorCode500Enum.choices",
+        },
     }
